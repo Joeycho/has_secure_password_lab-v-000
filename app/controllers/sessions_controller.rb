1aefbf73c76f17d8c5a000ea7d1b21fbd41469a0
Joey Cho <cjy12182000@naver.com>
2019-02-27 12:58:52 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
   def create
+    binding.pry
     user = User.find_by(name: params[:name])
    authenticated = user.try(:authenticate, params[:password])
    return head(:forbidden) unless authenticated
    @user = user
    session[:user_id] = @user.id
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
