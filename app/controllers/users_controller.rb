f1ed9eb69830e09c306cb3c5c26d5b0e0e2683d2
Joey Cho <cjy12182000@naver.com>
2019-02-27 13:14:27 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def create
-   @user = User.create(user_params)
-   session[:user_id] = @user.id
+    if user_params[:password]== user_params[:password_confirmation]
+     @user = User.create(user_params)
+     session[:user_id] = @user.id
+    else
+      redirect_to :new
   end
 
  private
