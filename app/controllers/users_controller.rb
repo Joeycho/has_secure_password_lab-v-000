6a32d7f3670ca5adafc65b84cd234794e9e41b32
Joey Cho <cjy12182000@naver.com>
2019-02-27 12:54:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def create
-  end
+   User.create(user_params)
+ end
+
+ private
+
+ def user_params
+   params.require(:user).permit(:name, :password, :password_confirmation)
+ end
 end
